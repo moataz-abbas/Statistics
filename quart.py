26cb8a9d471c544812ce1638c32c18342616c4af
moataz-abbas <53493536+moataz-abbas@users.noreply.github.com>
2021-07-19 04:32:31 +0300

    Add files via upload

--- quart.py
+++ quart.py
@@ -10,40 +10,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 d ='C:\Data-Science-with-Python-master\Chapter01\Data\Banking_Marketing.csv'
-dx2 = 'C:\Data-Science-with-Python-master\Chapter01\Data\Student_bucketing.csv'
+dx2 = 'C:/Users/mizo_/Documents/abbasmd.com/workstation/Data science in python/Data-Science-with-Python-master/Data-Science-with-Python-master/Chapter01/german_credit_data.csv'
 df = pd.read_csv(dx2, header = 0)
 
-print(df.dtypes)
+#print(df.dtypes)
 
-d1 = list(df.marks.dropna())
+d1 = list(df.Age.dropna())
 d2 = [71, 70, 90, 70, 70, 60, 70, 72, 72, 320, 71, 69]
-x = sorted (d1)
-#print (x)
+d3 = [51, 17, 25, 39, 7, 49, 62, 41, 20, 6, 43, 13]
+d4 = [ 6, 7, 15, 36, 39, 40, 41, 42, 43, 47, 49]
+#z = pd.DataFrame(d4)
+
+
+x = sorted (d4)
+print (x)
 print (f"len of data = {len(x)}")
 
 
-a,b,c,d = st.median_iqr(x)
+a,b,c,d = st.median_iqr_plt(x)
 q2 = st.median(x)
-print (f"q2 = {q2}")
-plt.axhline(q2, color= 'r', linestyle = '--', label = 'median' )
+#Q2 = z.quantile(0.5)
+#print(Q2)
 
+plt.axhline(q2, color= 'y', linestyle = '--', label = 'median' )
 
 
 qq1 = x[a:b+1]
 # print (qq1)
 q1 = st.median(qq1)
-print (f"q1 = {q1}")
+#Q1 = z.quantile(0.25)
+# print(Q1)
 plt.axhline(q1, color= 'r', linestyle = '--', label = 'q1' )
 
 
 qq3 = x[c:d+1]
 q3 =  st.median(qq3)
-print (f"q3 = {q3}")
+#Q3 = z.quantile(0.25)
+# print(Q3)
 plt.axhline(q3, color= 'g', linestyle = '--', label = 'q3' )
 
 
 iqr = q3-q1
-print (f"IQR = {iqr}")
+#IQR = Q3-Q1
+#print(IQR)
+
 
 lw = q1 - 1.5 * iqr
 uw = q3 + 1.5 * iqr
@@ -53,15 +63,17 @@ if lw < min(x):
 if uw > max(x):
     uw = max(x)
     
-
-
-
-print (f"lower whisker = {lw}")
-print (f"upper whisker = {uw}")
 plt.axhline(lw, color= 'b', linestyle = '--', label = 'lw' )
 plt.axhline(uw, color= 'k', linestyle = '--', label = 'uw' )
 
-print(min(x), max(x))
+print (f"lower whisker = {lw}")
+print (f"q1 = {q1}")    
+print (f"q2 = {q2}")
+print (f"q3 = {q3}")
+print (f"upper whisker = {uw}")
+
+print (f"IQR = {iqr}")
+# print(min(x), max(x))
 
 plt.boxplot(x)
 plt.legend()
