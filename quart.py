090fec117d4c26b96d65501419d92ea7cb4fdc7f
moataz-abbas <53493536+moataz-abbas@users.noreply.github.com>
2021-07-19 06:03:25 +0300

    Add files via upload

--- quart.py
+++ quart.py
@@ -19,62 +19,66 @@ d1 = list(df.Age.dropna())
 d2 = [71, 70, 90, 70, 70, 60, 70, 72, 72, 320, 71, 69]
 d3 = [51, 17, 25, 39, 7, 49, 62, 41, 20, 6, 43, 13]
 d4 = [ 6, 7, 15, 36, 39, 40, 41, 42, 43, 47, 49]
-#z = pd.DataFrame(d4)
+d5 = [82,76,24,40,67,69,62,75,78,71,32,98,89,78,67,72,82,87,66,56,52]
 
-
-x = sorted (d4)
+g = d5
+x = sorted (g)
+z = pd.DataFrame(g)
 print (x)
 print (f"len of data = {len(x)}")
 
 
 a,b,c,d = st.median_iqr_plt(x)
+# print(x[d])
+print(st.median_iqr_plt(x))
 q2 = st.median(x)
-#Q2 = z.quantile(0.5)
-#print(Q2)
-
-plt.axhline(q2, color= 'y', linestyle = '--', label = 'median' )
+Q2 = z.quantile(0.5)
+plt.axhline(q2, color= 'y', linestyle = '--', label = 'Median' )
 
 
 qq1 = x[a:b+1]
 # print (qq1)
 q1 = st.median(qq1)
-#Q1 = z.quantile(0.25)
-# print(Q1)
-plt.axhline(q1, color= 'r', linestyle = '--', label = 'q1' )
+Q1 = z.quantile(0.25)
+plt.axhline(q1, color= 'r', linestyle = '--', label = 'Q1' )
 
 
 qq3 = x[c:d+1]
 q3 =  st.median(qq3)
-#Q3 = z.quantile(0.25)
-# print(Q3)
-plt.axhline(q3, color= 'g', linestyle = '--', label = 'q3' )
+Q3 = z.quantile(0.75)
+plt.axhline(q3, color= 'g', linestyle = '--', label = 'Q3' )
 
 
 iqr = q3-q1
-#IQR = Q3-Q1
+IQR = Q3-Q1
 #print(IQR)
 
 
 lw = q1 - 1.5 * iqr
 uw = q3 + 1.5 * iqr
 
-if lw < min(x):
+if lw <= min(x):
     lw = min(x)
-if uw > max(x):
+if uw >= max(x):
     uw = max(x)
     
-plt.axhline(lw, color= 'b', linestyle = '--', label = 'lw' )
-plt.axhline(uw, color= 'k', linestyle = '--', label = 'uw' )
+plt.axhline(lw, color= 'b', linestyle = '--', label = 'Low whisker' )
+plt.axhline(uw, color= 'k', linestyle = '--', label = 'Upper whisker' )
 
 print (f"lower whisker = {lw}")
-print (f"q1 = {q1}")    
+print (f"q1 = {q1}")   
+print(Q1) 
 print (f"q2 = {q2}")
+print(Q2)
 print (f"q3 = {q3}")
+print(Q3)
 print (f"upper whisker = {uw}")
+#print(min(x), max(x))
 
 print (f"IQR = {iqr}")
-# print(min(x), max(x))
+print(IQR)
 
-plt.boxplot(x)
+
+plt.boxplot(x, whis= 1.5)
 plt.legend()
 plt.show()
